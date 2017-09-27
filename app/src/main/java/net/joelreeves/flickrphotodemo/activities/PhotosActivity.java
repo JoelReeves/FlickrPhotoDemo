4866da6258962a1d38d201ebb1b88f70afba980a
joelreeves <jmrmb80@gmail.com>
2017-09-26 20:11:10 -0400

    initial UI creation

--- app/src/main/java/net/joelreeves/flickrphotodemo/activities/PhotosActivity.java
+++ app/src/main/java/net/joelreeves/flickrphotodemo/activities/PhotosActivity.java
@@ -2,13 +2,26 @@ package net.joelreeves.flickrphotodemo.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BaseTransientBottomBar;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import net.joelreeves.flickrphotodemo.R;
+import net.joelreeves.flickrphotodemo.utils.NetworkUtils;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PhotosActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.recyclerview) RecyclerView recyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,5 +29,54 @@ public class PhotosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photos);
 
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getRecentPhotos();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                getRecentPhotos();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void getRecentPhotos() {
+        if (!NetworkUtils.networkIsAvailable(this)) {
+            showNoNetworkSnackbar();
+        } else {
+            // TODO
+        }
+    }
+
+    private void showNoNetworkSnackbar() {
+        Snackbar snackbar = Snackbar.make(toolbar, R.string.network_error_no_network_connection, BaseTransientBottomBar.LENGTH_INDEFINITE);
+        snackbar.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.red));
+        snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.white));
+        snackbar.setAction("Retry", noNetworkClickListener);
+        snackbar.show();
+    }
+
+    private final View.OnClickListener noNetworkClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            getRecentPhotos();
+        }
+    };
 }
---
+++ app/src/main/java/net/joelreeves/flickrphotodemo/utils/NetworkUtils.java
@@ -0,0 +1,22 @@
+package net.joelreeves.flickrphotodemo.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.support.annotation.NonNull;
+
+public final class NetworkUtils {
+
+    private NetworkUtils() {
+        throw new AssertionError("No instances.");
+    }
+
+    public static boolean networkIsAvailable(@NonNull Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm != null &&
+                cm.getActiveNetworkInfo() != null &&
+                cm.getActiveNetworkInfo().isAvailable() &&
+                cm.getActiveNetworkInfo().isConnected();
+    }
+}
