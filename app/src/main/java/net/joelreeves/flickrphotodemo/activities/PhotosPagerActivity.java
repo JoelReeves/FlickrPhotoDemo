c53abb33fb68ad70c9e254464d330a385b891c84
joelreeves <jmrmb80@gmail.com>
2017-09-27 21:59:14 -0400

    wip showing clicked image in second activity

--- app/src/main/java/net/joelreeves/flickrphotodemo/activities/PhotosPagerActivity.java
+++ app/src/main/java/net/joelreeves/flickrphotodemo/activities/PhotosPagerActivity.java
@@ -2,20 +2,30 @@ package net.joelreeves.flickrphotodemo.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import net.joelreeves.flickrphotodemo.R;
 import net.joelreeves.flickrphotodemo.application.FlickrDemoApplication;
 import net.joelreeves.flickrphotodemo.models.Photo;
+import net.joelreeves.flickrphotodemo.views.CropCircleTransformation;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PhotosPagerActivity extends AppCompatActivity {
 
+    @BindView(R.id.photo_detail) ImageView currentPhotoImageView;
+
     private static final String PHOTO_EXTRA = "photo_extra";
+    private static final int PHOTO_DIMENSIONS = 400;
 
     private Photo currentPhoto;
 
@@ -37,7 +47,15 @@ public class PhotosPagerActivity extends AppCompatActivity {
         currentPhoto = getIntent().getParcelableExtra(PHOTO_EXTRA);
 
         if (currentPhoto != null) {
-            // TODO
+            final String photoUrl = TextUtils.isEmpty(currentPhoto.getUrl()) ? "" : currentPhoto.getUrl();
+            Picasso.with(this)
+                    .load(Uri.parse(photoUrl))
+                    .placeholder(R.drawable.ic_photo_loading)
+                    .error(R.drawable.ic_photo_error)
+                    .resize(PHOTO_DIMENSIONS, PHOTO_DIMENSIONS)
+                    .transform(new CropCircleTransformation())
+                    .centerCrop()
+                    .into(currentPhotoImageView);
         }
     }
 }
